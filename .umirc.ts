f5ebd9319f518b24a986c62451423cdad59e26b8
codingcat <istommao@gmail.com>
2022-07-28 17:32:39 +0800

    Init Project

---
+++ .umirc.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'dumi';
+
+export default defineConfig({
+  title: 'CodeSnippet',
+  favicon: 'https://c.codingcat.top/static/imgs/cat1.png',
+  logo: 'https://c.codingcat.top/static/imgs/cat1.png',
+  outputPath: 'docs-dist',
+  mode: 'site',
+  // more config: https://d.umijs.org/config
+});
